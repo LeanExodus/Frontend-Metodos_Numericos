c399667ce3a88601998a5439f5c67c03b3f4a2aa
Leandro Campos <leandro-1018@hotmail.com>
2023-08-04 23:04:45 -0600

    cambios modal

--- components/public/NewtonForm.tsx
+++ components/public/NewtonForm.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useRef, useState } from 'react'
 import { AppContext } from '../context/appContext'
 import { SessionContext } from '@/components/context/sessionContext'
-import { errorModal, guideModal } from '@/helpers/modals'
+import { errorModal, guideModalNewton } from '@/helpers/modals'
 import axios from 'axios'
 import Geogebra from 'react-geogebra'
 
@@ -172,7 +172,7 @@ export const NewtonForm = () => {
                         </div>
 
                         <div className='grid grid-cols-2 gap-4'>
-                            <button type='button' onClick={guideModal} className='flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600'> Guia</button>
+                            <button type='button' onClick={guideModalNewton} className='flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600'> Guia</button>
                             <button type='submit' className='flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'> Calcular</button>
                        </div>
                     </form>
--- helpers/modals.ts
+++ helpers/modals.ts
@@ -1,5 +1,37 @@
 import Swal, { SweetAlertResult } from 'sweetalert2';
 
+let htmlContent = `<ul>
+<li><b>Para el campo de la función se tiene que tomar en cuenta:</b><br>
+&nbsp;&nbsp;&nbsp;&#8226 Al querer multiplicar por x se debe ingresar el símbolo * antes de la x<br>
+&nbsp;&nbsp;&nbsp;&#8226 Para ingresar una raíz cuadrada se usa sqrt(<i><b>Numero al cual queremos sacar la raiz</b></i>)</li>
+<li><b>Para los campos del intervalo:</b><br>
+&nbsp;&nbsp;&nbsp;&#8226 <i> Estos campos no son requeridos</i>, ya que si no se ingresan la aplicación calculará el intervalo mediante Bolzano<br>
+&nbsp;&nbsp;&nbsp;&#8226 Si vas a ingresarlos, procura que entre esos puntos exista una raíz, de lo contrario no se calculará</li>
+<li><b>El campo de la tolerancia:</b><br>
+&nbsp;&nbsp;&nbsp;&#8226 Este campo determina el nivel de precisión requerida<br>
+&nbsp;&nbsp;&nbsp;&#8226 Se recomienda usar valores cercanos a 0<br>
+&nbsp;&nbsp;&nbsp;&#8226 No se admiten valores negativos.</li>
+</ul>`
+
+let htmlContentNewton =
+
+`<ul>
+En este metodo puedes usar el intervalo o el valor inicial (<i>Si ya sabes el punto medio</i>)
+<li><b>Para el campo de la función se tiene que tomar en cuenta:</b><br>
+&nbsp;&nbsp;&nbsp;&#8226 Al querer multiplicar por x se debe ingresar el símbolo * antes de la x<br>
+&nbsp;&nbsp;&nbsp;&#8226 Para ingresar una raíz cuadrada se usa sqrt(<i><b>Numero al cual queremos sacar la raiz</b></i>)</li>
+<li><b>Para el campo de valor inicial:</b><br>
+&nbsp;&nbsp;&nbsp;&#8226 <i> Este campo no es requerido </i><br>
+&nbsp;&nbsp;&nbsp;&#8226 Este campo es dado por la suma de los puntos de intervalo divido entre 2 (<i>Punto Medio</i>)<br>
+&nbsp;&nbsp;&nbsp;&#8226 Usalo solo si tienes conocimiento del punto medio o si el problema te lo indica</li>
+<li><b>Para los campos del intervalo:</b><br>
+&nbsp;&nbsp;&nbsp;&#8226 <i> Estos campos no son requeridos</i>, ya que si no se ingresan la aplicación calculará el intervalo mediante Bolzano<br>
+&nbsp;&nbsp;&nbsp;&#8226 Si vas a ingresarlos, procura que entre esos puntos exista una raíz, de lo contrario no se calculará</li>
+<li><b>El campo de la tolerancia:</b><br>
+&nbsp;&nbsp;&nbsp;&#8226 Este campo determina el nivel de precisión requerida<br>
+&nbsp;&nbsp;&nbsp;&#8226 Se recomienda usar valores cercanos a 0<br>
+&nbsp;&nbsp;&nbsp;&#8226 No se admiten valores negativos.</li>
+</ul>`
 
 export const errorModal = (text: string) => {
 	text = Array.isArray(text) ? text[0].error : text;
@@ -19,19 +51,21 @@ export const errorModal = (text: string) => {
 export const guideModal = () => {
 
 	Swal.fire({
-		html:
-		`<ul>
-		<li>Para el campo de la función se tiene que tomar en cuenta:<br>
-		   Al querer multiplicar por x se debe ingresar el símbolo * antes de la x<br>
-		   Para ingresar una raíz cuadrada se usa sqrt(<i>Numero al cual queremos sacar la raiz</i>)</li>
-		<li>Para los campos del intervalo:<br>
-		    Estos campos no son requeridos, ya que si no se ingresan la aplicación calculará el intervalo mediante Bolzano<br>
-		    Si vas a ingresarlos, procura que entre esos puntos exista una raíz, de lo contrario no se calculará</li>
-		<li>El campo de la tolerancia:<br>
-		    Este campo determina el nivel de precisión requerida<br>
-		    Se recomienda usar valores cercanos a 0<br>
-		    No se admiten valores negativos.</li>
-	  </ul>`,
+		html:htmlContent,
+		icon: 'info',
+		width: 1000,
+		confirmButtonText: 'Entendido',
+		customClass: {
+			popup: 'modal-popup',
+			confirmButton: 'modal-confirm-button',
+		},
+	});
+};
+
+export const guideModalNewton = () => {
+
+	Swal.fire({
+		html:htmlContentNewton,
 		icon: 'info',
 		width: 1000,
 		confirmButtonText: 'Entendido',
