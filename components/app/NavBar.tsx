53f2815d97b2b52dbea3ff7190f4a5c655e95b43
Victor Zuniga <victorandres1218@gmail.com>
2023-08-11 15:26:49 -0600

    Divisiones Navbar

--- components/app/NavBar.tsx
+++ components/app/NavBar.tsx
@@ -3,25 +3,31 @@ import React, { useContext } from 'react'
 import { SessionContext } from '../context/sessionContext'
 
 export const NavBar = () => {
-    const { logOut } = useContext(SessionContext)
+    const { logOut } = useContext(SessionContext);
+
     return (
         <nav className="bg-gray-800">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="relative flex h-16 items-center justify-between">
-                    <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-center">
+                    <div className="flex items-center justify-start">
+                        <Link href="/app" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-base font-medium">Inicio</Link>
+                    </div>
 
-
-                        <div className="flex space-x-4">
-                            <Link className='text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-base font-medium' href={"/app"}>Inicio</Link>
-                            <Link className='text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-base font-medium' href={"/app/newton"}>Newton</Link>
-                            <Link className='text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-base font-medium' href={"/app/secant"}>Secante</Link>
-                            <Link className='text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-base font-medium' href={"/app/about"}>Nosotros</Link>
-                            <button className='text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-base font-medium' onClick={() => { logOut() }}>Salir</button>
+                    <div className="flex items-center justify-center">
+                        <div className="flex items-center justify-center space-x-4">
+                            <Link href="/app/newton" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-base font-medium">Newton</Link>
+                            <div className="border-r-2 border-white h-6"></div>
+                            <Link href="/app/secant" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-base font-medium">Secante</Link>
                         </div>
+                    </div>
 
+                    <div className="flex items-center justify-end">
+                        <Link href="/app/about" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-base font-medium pr-3">Nosotros</Link>
+                        <div className="border-r-2 border-white h-6"></div>
+                        <button className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-base font-medium" onClick={() => { logOut() }}>Salir</button>
                     </div>
                 </div>
             </div>
         </nav>
-    )
-}
+    );
+};
